56f8d0162e9d3ce084f0aede069f280a4807c9a3
midori4 <39873009+midori4@users.noreply.github.com>
2020-08-01 17:11:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-(function(){
+(async function(){
     console.log("hello treasure");
+    let localStream;
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, voice: true });
+        const videoElement = document.getElementById('my-video');
+        videoElement.srcObject = localStream;
+        videoElement.play();
+    } catch(error) {
+        alert(error);
+    }
+    const peer = new Peer({
+        key: '8be054a1-1cdf-424c-b1a3-0f126be818df',
+        debug: 3
+    });
+    // PeerID取得
+    peer.on('open', () => {
+        document.getElementById('my-id').textContent = peer.id;
+    });
+    // 発信処理
+    document.getElementById('make-call').onclick = () => {
+        const theirID = document.getElementById('their-id').value;
+        const mediaConnection = peer.call(theirID, localStream);
+        setEventListener(mediaConnection);
+    };
+    // イベントリスナ
+    const setEventListener = mediaConnection => {
+        mediaConnection.on('stream', stream => {
+            const videoElement = document.getElementById('their-video');
+            videoElement.srcObject = stream;
+            videoElement.play();
+        });
+    };
+    // 着信処理
+    peer.on('call', mediaConnection => {
+        mediaConnection.answer(localStream);
+        setEventListener(mediaConnection);
+    });
 })();
